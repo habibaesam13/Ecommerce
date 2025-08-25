b07734262273ed9c9c5e1328479effc3bc3cdee4
Habiba <habebaesam13@gmail.com>
2025-08-25 15:41:58 +0300

    fav addeed to products page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,27 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favourite;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Services\ProductService;
 use App\Services\CategoryService;
+use App\Services\FavouriteService;
 
 class ProductController extends Controller
 {
 
     private ProductService $productService;
     private CategoryService $categoryService;
+    private FavouriteService $favouriteService;
 
-    public function __construct(ProductService $productService, CategoryService $categoryService)
+    public function __construct(ProductService $productService, CategoryService $categoryService, FavouriteService $favouriteService)
     {
         $this->productService = $productService;
         $this->categoryService = $categoryService;
+        $this->favouriteService = $favouriteService;
     }
 
-    public function index(Request $request, ProductService $service)
+    public function index(Request $request)
     {
-        $products = $service->index($request->q);
-        return view('products.index', compact('products'));
+        $products = $this->productService->index($request->q);
+        $favourites = $this->favouriteService->getFavouritesByUserId(auth()->id());
+        return view('products.index', compact('products', 'favourites'));
     }
 
 
--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -10,41 +10,58 @@
     <div class="row">
         @forelse ($products as $product)
         <div class="col-md-3 col-sm-6 mb-4">
-            <div class="product-card">
-                <div class="product-img-wrapper">
-                    <img src="{{ asset('storage/'.$product->img) }}" alt="{{ $product->name }}" loading="lazy">
-                    @if($product->discount_amount > 0)
-                        <div class="triangle-discount">
-                            <span>-{{ (int) $product->discount_amount }}%</span>
-                        </div>
-                    @endif
-                </div>
-
-                <div class="card-body d-flex flex-column">
-                    <h6 class="card-title">{{ $product->name }}</h6>
-                    <p class="card-text text-muted mb-2">
-                        {{ Str::limit($product->description, 80) }}
-                    </p>
-
-                    <div class="wishlist-price d-flex align-items-center gap-2">
+            <a href="{{ route('product-details', $product->id) }}" class="text-decoration-none">
+                <div class="product-card h-100">
+                    {{-- Favorite Icon --}}
+                    @php
+                    $isFavourite = $favourites->contains('id', $product->id);
+                    @endphp
+                    <form action="{{ route('favourites.toggle') }}" method="POST" class="favorite-form">
+                        @csrf
+                        <input type="hidden" name="product_id" value="{{ $product->id }}">
+                        <button type="submit" class="btn btn-link p-0">
+                            @if($isFavourite)
+                            <i class="fa fa-heart favorite-icon text-danger"></i>
+                            @else
+                            <i class="fa fa-heart favorite-icon text-secondary"></i>
+                            @endif
+                        </button>
+                    </form>
+                    <div class="product-img-wrapper">
+                        <img src="{{ asset('storage/'.$product->img) }}" alt="{{ $product->name }}" loading="lazy">
                         @if($product->discount_amount > 0)
-                            <span class="old-price">${{ number_format($product->price, 2) }}</span>
-                            <span class="new-price">${{ number_format($product->final_price, 2) }}</span>
-                        @else
-                            <span class="new-price">${{ number_format($product->price, 2) }}</span>
+                            <div class="triangle-discount">
+                                <span>-{{ (int) $product->discount_amount }}%</span>
+                            </div>
                         @endif
                     </div>
 
-                    <p class="stock">
-                        {{ $product->stock > 0 ? 'In stock - ready to ship' : 'Out of stock' }}
-                    </p>
+                    <div class="card-body d-flex flex-column">
+                        <h6 class="card-title">{{ $product->name }}</h6>
+                        <p class="card-text text-muted mb-2">
+                            {{ Str::limit($product->description, 80) }}
+                        </p>
 
-                    {{-- Action Button --}}
-                    <div class="d-flex justify-content-center gap-2">
-                        <a href="#" class="btn cart w-50">Add to Cart</a>
+                        <div class="wishlist-price d-flex align-items-center gap-2">
+                            @if($product->discount_amount > 0)
+                                <span class="old-price">${{ number_format($product->price, 2) }}</span>
+                                <span class="new-price">${{ number_format($product->final_price, 2) }}</span>
+                            @else
+                                <span class="new-price">${{ number_format($product->price, 2) }}</span>
+                            @endif
+                        </div>
+
+                        <p class="stock">
+                            {{ $product->stock > 0 ? 'In stock - ready to ship' : 'Out of stock' }}
+                        </p>
+
+                        {{-- Action Button --}}
+                        <div class="d-flex justify-content-center gap-2">
+                            <a href="#" class="btn cart w-50">Add to Cart</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
         @empty
         <p class="text-center">No products available.</p>
